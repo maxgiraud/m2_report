ed5c32f271bbc3413948e7eec6937b67fa140d3a
mgiraud <maximegiraud4@gmail.com>
2018-04-09 10:33:15 +0200

    upload

---
+++ xrun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pdflatex rapport.tex \
+&& bibtex rapport.aux \
+&& pdflatex rapport.tex \
+&& pdflatex rapport.tex \
+&& rm -v *.aux *.bbl *.blg *.lof *.log *.out *.toc \
+&& evince rapport.pdf
